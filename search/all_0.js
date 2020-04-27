3393691d9dd58e5df541ba1fcedf8033aedfece7
Carlos <alu0101102726@ull.edu.es>
2020-04-27 16:46:45 +0100

    mejoras generales

--- search/all_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['algorithm_0',['Algorithm',['../class_algorithm.html',1,'']]],
-  ['arbol_20de_20directorios_1',['Arbol de directorios',['../md__r_e_a_d_m_e.html',1,'']]]
-];
--- search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['arbol_20de_20directorios_94',['Arbol de directorios',['../md__r_e_a_d_m_e.html',1,'']]]
-];
